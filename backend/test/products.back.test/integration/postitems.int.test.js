987faf93cfbcb7e9da5bca46f4604e4d84b8085a
navi5971 <omntathsiluni@students.nsbm.ac.lk>
2024-11-29 12:31:47 +0530

    test edit

--- backend/test/products.back.test/integration/postitems.int.test.js
+++ backend/test/products.back.test/integration/postitems.int.test.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const request = require("supertest");
 const mongoose = require("mongoose");
-const server = require("../../../server.js"); // Ensure this path is correct to your server file
-const Item = require("../../../models/item.js"); // Ensure this path is correct to your Item model
-const itemRouter = require("../../../routes/itemroutes.js"); // Ensure this path is correct
+const server = require("../../../server.js"); 
+const Item = require("../../../models/item.js"); 
+const itemRouter = require("../../../routes/itemroutes.js"); 
 const sinon = require('sinon');
 const { addItem } = require('../../../controllers/itemcont.js');
 
-// Replace this with your actual Atlas test database connection string
+//Atlas test database connection string
 const mongoURL = "mongodb+srv://Navithma:Navithma78@cluster1.gqwja.mongodb.net/testdb?retryWrites=true&w=majority";
 
 
@@ -29,8 +29,8 @@ describe("API Endpoint Tests", () => {
   it("Should successfully add a new item and return it with a 201 status", async () => {
     const newItem = {
         name: "Hat",
-        variants: ["small", "medium", "large"], // Matches the format from the database
-        prices: { // Converted to the object format as per your database
+        variants: ["small", "medium", "large"], 
+        prices: { 
           small: 10,
           medium: 15,
           large: 20
@@ -43,15 +43,15 @@ describe("API Endpoint Tests", () => {
         rating: 0
       };
       
-    const response = await request(server) // Use the Express app server instance
-      .post("/api/items/items") // Replace with your actual endpoint for adding an item
+    const response = await request(server) //Express app server instance
+      .post("/api/items/items") // endpoint for adding an item
       .send(newItem);
 
     expect(response.status).toBe(201);
     expect(response.body).toMatchObject({
         name: "Hat",
-        variants: ["small", "medium", "large"], // Matches the format from the database
-        prices: { // Converted to the object format as per your database
+        variants: ["small", "medium", "large"], 
+        prices: { 
           small: 10,
           medium: 15,
           large: 20
@@ -91,7 +91,7 @@ describe('API Endpoint Tests', () => {
       };
   
       const response = await request(server)
-        .post('/api/items/items') // Adjust the endpoint if needed
+        .post('/api/items/items') 
         .send(newItem);
   
       expect(response.status).toBe(400); // Expect 500 status
