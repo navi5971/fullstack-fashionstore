65211ca15e99c5cc83534d37e2ee8cb9204e5ba7
Tharindu Damintha <ovtdamintha@gmail.com>
2024-12-04 20:18:38 +0530

    Material-UI-setup

--- fashion-front/fashion-store/src/index.js
+++ fashion-front/fashion-store/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 // Create a React root
 const root = ReactDOM.createRoot(rootElement);
