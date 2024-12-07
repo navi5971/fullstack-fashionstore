f303f9a4d006986638d622261443a8eb0c0df30f
Tharindu Damintha <ovtdamintha@gmail.com>
2024-12-07 14:35:15 +0530

    Implemented-sample-404-page

--- fashion-front/fashion-store/src/App.js
+++ fashion-front/fashion-store/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./screens/home";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
@@ -14,29 +17,28 @@ import CollectionsList from "./components/collectionlist";
 import ProductForm from "./components/product/productfrom";
 import Cartscreen from "./screens/cartscreen";
 import Payment from "./screens/paymentform";
-import Wishlist from "./screens/wishlist";
+import { NotFound } from "./screens/404";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-       
-      <Navbar />
-          <Routes>
-            <Route path="/" exact element={<Home />} />
-            <Route path='/search' exact element={<Search/>}/>
-            <Route path="/search-results/:category" element={<SearchResults />} />
-            <Route path='/dash' element={<Dash/>}/>
-            <Route path='/additem' element={<ProductForm/>}/>
-            <Route path='/collection' element={<CollectionsList/>}/>
-            <Route path='/cart' element={<Cartscreen/>}/>
-            <Route path = '/payment' element={<Payment/>}/>
-            <Route path="" element /> {/* add the 404 page*/}
-          </Routes>
-        
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/search" exact element={<Search />} />
+          <Route path="/search-results/:category" element={<SearchResults />} />
+          <Route path="/dash" element={<Dash />} />
+          <Route path="/additem" element={<ProductForm />} />
+          <Route path="/collection" element={<CollectionsList />} />
+          <Route path="/cart" element={<Cartscreen />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
